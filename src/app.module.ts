305d5b6f55c576d301965ca8932d7364729c51c1
mirelest <kiwijesustm@gmail.com>
2026-06-17 19:46:25 -0600

    add: Uso de prisma ORM y configuración de servicios y modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/infrastructure/tasks.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     TasksModule
   ],
 })
--- src/tasks/infrastructure/controllers/dtos/create-task.dto.ts
+++ src/tasks/infrastructure/controllers/dtos/create-task.dto.ts
@@ -1,12 +1,25 @@
+import { ApiAcceptedResponse, ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateTaskDto {
+     
+    @ApiProperty({ 
+        description: 'Descripción general de la tarea',
+        example: 'This is a description',
+        required: true,
+    })
 
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
     @MaxLength(100)
     title!: string;
+
+    @ApiProperty({ 
+        description: 'Descripción general de la tarea',
+        example: 'This is a description',
+        required: true,
+    })
     
     @IsString()
     @IsNotEmpty()
